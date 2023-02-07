2aa8653adb40bac44a8cc7a0a1ac57439fd414fa
priyanka_kothule <99719180+23priya@users.noreply.github.com>
2023-02-07 17:07:47 +0530

    Add files via upload

---
+++ class.java
@@ -0,0 +1,27 @@
+claSS Myclass
+{
+    private int data;
+            //getter method----read access
+    int getData()
+    {
+        return data;
+
+    }
+         //setter method-----write access
+    void setData(int data)
+    {
+        if()
+        this.data = data;
+     /
+    }
+}
+
+public calss Home
+{
+    public static void main(string[] args)
+    {
+        Myclass m1 = new Myclass();
+
+        System.out.println(m1.getData())
+    }
+}
